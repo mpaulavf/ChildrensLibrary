494a57776a716b346dfd20fce87bb7950e06e773
dpenap <dpenap@unal.edu.co>
2020-04-26 00:10:30 -0500

    Reemplazar Stack por Duracell, una pila con arreglos genéricos. Los archivos adicionales de idea son de mi IDE

---
+++ src/basicData/Duracell.java
@@ -0,0 +1,48 @@
+package basicData;
+
+public class Duracell<T> {
+    private int top;
+    private int size;
+    private T[] ar;
+
+    public Duracell(int size){
+        this.size = size;
+        top = 0;
+        ar = new T[size];
+    }
+
+    public Duracell(){
+        size = 10;
+        top = 0;
+        ar = new T[size];
+    }
+
+    public boolean isFull (){
+        return (top == size - 1);
+    }
+
+    public boolean isEmpty(){
+        return (top == 0);
+    }
+
+    public void push (T item) throws Exception{
+        if (isFull()) throw new Exception("La pila está llena");
+
+        ar[top] = item;
+        top++;
+
+    }
+
+    public T pop () throws Exception{
+        if(isEmpty())throw new Exception("La pila está vacía");
+
+        top --;
+        return ar[top];
+    }
+
+    public T peek() throws Exception{
+        if(isEmpty())throw new Exception("La pila está vacía");
+
+        return ar[top-1];
+    }
+}
