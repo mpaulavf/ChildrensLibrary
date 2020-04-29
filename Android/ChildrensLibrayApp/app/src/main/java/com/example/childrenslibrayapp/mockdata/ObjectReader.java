9eaf552a241dbe2ad63d478640bc4707748e936c
dpenap <dpenap@unal.edu.co>
2020-04-29 17:30:39 -0500

    Añadida funcionalidad para importar como Lista o como Arr Dinamico

--- Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/mockdata/ObjectReader.java
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/mockdata/ObjectReader.java
@@ -22,15 +22,13 @@ import java.lang.*;
 
 public class ObjectReader {
     private Context context;
-    private DynamicArray<Book> libros = new DynamicArray<Book>();
-    private DynamicArray<User> users = new DynamicArray<User>();
 
     public ObjectReader(Context current){
         this.context = current;
     }
 
 
-    public void readBooks() {
+    public void readBooks(DynamicArray<Book> libros) {
         //Basado en info dada en https://www.youtube.com/watch?v=i-TqNzUryn8
         InputStream is = context.getResources().openRawResource(R.raw.libros_x10000);
 
@@ -56,7 +54,33 @@ public class ObjectReader {
         }
     }
 
-    public void readUsers(){
+    public void readBooks(SinglyLinkedList<Book> libros) {
+        //Basado en info dada en https://www.youtube.com/watch?v=i-TqNzUryn8
+        InputStream is = context.getResources().openRawResource(R.raw.libros_x10000);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((is)));
+
+        String line = "";
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split(",");
+                Book book = new Book();
+                book.setTitle(tokens[0]);
+                book.setAuthorName(tokens[1]);
+                book.setCode(tokens[2]);
+                book.setGenre(tokens[3]);
+
+                libros.insertNodeAtTail(book);
+
+                Log.d("ObjectGenerator", "Libro creado: " + book);
+            }
+        }catch (IOException e){
+            Log.wtf("ObjectGenerator", "Error reading data file " + line, e);
+            e.printStackTrace();
+        }
+    }
+
+    public void readUsers(DynamicArray<User> users){
 
         //Basado en info dada en https://www.youtube.com/watch?v=i-TqNzUryn8
         InputStream is = context.getResources().openRawResource(R.raw.usuarios_x10000);
@@ -85,5 +109,36 @@ public class ObjectReader {
         }
 
     }
+    public void readUsers(SinglyLinkedList<User> users){
+
+        //Basado en info dada en https://www.youtube.com/watch?v=i-TqNzUryn8
+        InputStream is = context.getResources().openRawResource(R.raw.usuarios_x10000);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((is)));
+
+        String line = "";
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split(",");
+                User user = new User();
+                user.setName(tokens[0]);
+                user.setSurname(tokens[1]);
+                user.setNickname(tokens[2]);
+                user.setPassword(tokens[3]);
+
+                user.setIsWorker((Boolean.valueOf(tokens[4])));
+
+                users.insertNodeAtTail(user);
+
+                Log.d("ObjectGenerator", "Usuario creado: " + user);
+            }
+        }catch (IOException e){
+            Log.wtf("ObjectGenerator", "Error reading data file " + line, e);
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 }
