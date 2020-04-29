36a93847dbfc0ff80160f9b75e6055c39649570e
dpenap <dpenap@unal.edu.co>
2020-04-28 20:05:57 -0500

    cambios restantes

---
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/RecyclerViewAdaptador.java
@@ -0,0 +1,56 @@
+package com.example.childrenslibrayapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdaptador extends RecyclerView.Adapter<RecyclerViewAdaptador.ViewHolder>{
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView titulo, autor, genero, codigo;
+
+        private ImageView caratula;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titulo = (TextView) itemView.findViewById(R.id.tv_titulo);
+            autor = (TextView) itemView.findViewById(R.id.tv_autor);
+            genero = (TextView) itemView.findViewById(R.id.tv_genero);
+            codigo = (TextView) itemView.findViewById(R.id.tv_codigo);
+            caratula = (ImageView) itemView.findViewById(R.id.im_caratula);
+        }
+
+
+    }
+
+    public List <LibroEnLista> libroEnListas;
+
+    public RecyclerViewAdaptador(List<LibroEnLista> libroEnListas) {
+        this.libroEnListas = libroEnListas;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_libro, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.caratula
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
