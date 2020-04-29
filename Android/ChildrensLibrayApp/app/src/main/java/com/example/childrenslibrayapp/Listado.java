010c0c4ad23ce11da08a11507db635afb95fefd3
dpenap <dpenap@unal.edu.co>
2020-04-28 19:10:59 -0500

    faltaban archivos

---
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/Listado.java
@@ -0,0 +1,14 @@
+package com.example.childrenslibrayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+    }
+}
