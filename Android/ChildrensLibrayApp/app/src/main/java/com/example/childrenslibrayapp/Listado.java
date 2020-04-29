c3cbf838efb749aac86664e5785c4010d4c87d59
dpenap <dpenap@unal.edu.co>
2020-04-28 22:13:50 -0500

    Fallas por cambiar Listado a usar SinglyLinkedList

--- Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/Listado.java
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/Listado.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.childrenslibrayapp.objects.Book;
+import com.example.childrenslibrayapp.structures.SinglyLinkedList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,13 +30,13 @@ public class Listado extends AppCompatActivity {
 
     }
 
-    public List<LibroEnLista> obtenerlibro(){
-        List<LibroEnLista> libro= new ArrayList<>();
-        libro.add(new LibroEnLista("Un monstru viene a verme", "Patrick Ness","Accion", "0958458",R.drawable.libro1));
-        libro.add(new LibroEnLista("El principito", "Antoine de Saint-Exupéry","Accion", "0958457",R.drawable.libro2));
-        libro.add(new LibroEnLista("El maravilloso mago de Oz", "L. Frank Baum","Accion", "0958456",R.drawable.libro3));
-        libro.add(new LibroEnLista("Las aventuras de Pinocho", "Carlo Collodi","Accion", "0958455",R.drawable.libro4));
-        libro.add(new LibroEnLista("Apestoso tío Muffin", "Pedro Mañas","Accion", "0958454",R.drawable.libro5));
+    public SinglyLinkedList<LibroEnLista> obtenerlibro(){
+        SinglyLinkedList<LibroEnLista> libro= new SinglyLinkedList<LibroEnLista>();
+        libro.insertNodeAtHead(libro.head, new LibroEnLista("Un monstru viene a verme", "Patrick Ness","Accion", "0958458",R.drawable.libro1));
+        libro.insertNodeAtHead(libro.head, new LibroEnLista("El principito", "Antoine de Saint-Exupéry","Accion", "0958457",R.drawable.libro2));
+        libro.insertNodeAtHead(libro.head, new LibroEnLista("El maravilloso mago de Oz", "L. Frank Baum","Accion", "0958456",R.drawable.libro3));
+        libro.insertNodeAtHead(libro.head, new LibroEnLista("Las aventuras de Pinocho", "Carlo Collodi","Accion", "0958455",R.drawable.libro4));
+        libro.insertNodeAtHead(libro.head, new LibroEnLista("Apestoso tío Muffin", "Pedro Mañas","Accion", "0958454",R.drawable.libro5));
 
         return libro;
 
