b555132272de483e0078bb70106722ae0accf0b4
mpaulavf <60564903+mpaulavf@users.noreply.github.com>
2020-06-11 18:24:03 -0500

    meétodo insertBalance en ListBST donde se balance automáticamente al insertar un nuevo nodo

--- Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/structures/ListBST.java
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/structures/ListBST.java
@@ -68,7 +68,65 @@ public class ListBST { //implementado para enteros
             //reemplazar N con X, promover X.right;
         }
     }
-/*
+
+    public static ListBSTNode insertBalance(ListBSTNode root,int data){
+
+        if(root==null){
+            root = new ListBSTNode(data);
+            root.height = adjustRootHeight(root);
+            return root;
+        } else if(data<=root.data) root.left = insertBalance(root.left, data);
+        else if(data>root.data) root.right = insertBalance(root.right, data);
+
+        int balanceFactor = rootHeight(root.left) - rootHeight(root.right);
+
+        if(balanceFactor>1){
+            if(rootHeight(root.left.left) < rootHeight(root.left.right)) root.left = leftRotation(root.left);
+            root = rightRotation(root);
+        } else if(balanceFactor<-1){
+            if(rootHeight(root.right.right) < rootHeight(root.right.left)) root.right = rightRotation(root.right);
+            root = leftRotation(root);
+        } else root.height = adjustRootHeight(root);
+
+        return root;
+
+    }
+
+    public static int rootHeight(ListBSTNode root){
+        if(root==null) return -1;
+        else return root.height;
+    }
+
+    public static int adjustRootHeight(ListBSTNode root){
+        if(root==null) return -1;
+        else return 1 + Math.max(rootHeight(root.left), rootHeight(root.right));
+    }
+
+    public static ListBSTNode rightRotation(ListBSTNode root){
+
+        ListBSTNode temp = root.left;
+        root.left = temp.right;
+        temp.right = root;
+        root.height = adjustRootHeight(root);
+        temp.height = adjustRootHeight(temp);
+
+        return temp;
+
+    }
+
+    public static ListBSTNode leftRotation(ListBSTNode root){
+
+        ListBSTNode temp = root.right;
+        root.right = temp.left;
+        temp.left = root;
+        root.height = adjustRootHeight(root);
+        temp.height = adjustRootHeight(temp);
+
+        return temp;
+
+    }
+
+    /*
     private void rotateRight (ListBSTNode x){
         ListBSTNode p = x.parent;
         ListBSTNode y = x.left;
