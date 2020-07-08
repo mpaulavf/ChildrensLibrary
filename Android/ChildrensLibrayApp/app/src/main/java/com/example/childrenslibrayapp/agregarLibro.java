c0589b050bc9c604cf8cbc5baccc1687666dd8a4
tq08 <tanitagissellqg@gmail.com>
2020-07-08 13:25:45 -0500

    spinner2

--- Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/agregarLibro.java
+++ Android/ChildrensLibrayApp/app/src/main/java/com/example/childrenslibrayapp/agregarLibro.java
@@ -3,13 +3,24 @@ package com.example.childrenslibrayapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class agregarLibro extends AppCompatActivity {
+
+    Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_libro);
+
+        spinner2 =(Spinner)findViewById(R.id.spinner2);
+
+        String [] opciones = {"Comedia","Comic","Cuento", "Educacion","Fabula", "Ficcion", "Ilustrado", "Romantico"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, opciones);
+        spinner2.setAdapter(adapter);
     }
 
 
